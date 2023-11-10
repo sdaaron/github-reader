54504b76797ec62a6b261e4fa8650a94d6fab9f1
sdaaron <sidashu233@gmail.com>
2023-11-10 12:57:35 +0800

    update server.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def read_all_files(directory):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80)
